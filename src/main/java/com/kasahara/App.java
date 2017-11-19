cff4e114761df30c393889eeed8bf349a16d787b
Daisuke Kasahara <daiponpon0211@gmail.com>
2017-11-19 19:20:01 +0900

    初-お試しコミット

---
+++ src/main/java/com/kasahara/App.java
@@ -0,0 +1,24 @@
+package com.kasahara;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        new App().printMessage("Taro");
+    }
+
+    public String getMessage(String name){
+        return "Hi," + name + ". Welcome to Maven World!";
+    }
+
+    public void printMessage(String name){
+        System.out.println("¥n***** App class *****¥n");
+        System.out.println(this.getMessage(name));
+        System.out.println("¥n***** App class *****¥n");
+    }
+
+
+}
---
+++ src/test/java/com/kasahara/AppTest.java
@@ -0,0 +1,43 @@
+package com.kasahara;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+
+    private App app = null;
+
+    public AppTest( String testName ) {
+        super( testName );
+        app = new App();
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertNotNull( app );
+    }
+
+    public void testGetMessage(){
+        String name = "hoge";
+        String msg = "Hi," + name + ". Welcome to Maven World!";
+        assertEquals(app.getMessage("hoge"), msg);
+    }
+}
